e987d5b72820cd2f2d8392af09c9ea7e9e4e687a
tannmatter <tannmatter@gmail.com>
2023-07-17 12:00:41 -0500

    Add GetMatchingResponseName()
    Implemented response name matching in ToString() and added the missing success & failure responses.

--- Cave/src/DeviceControllers/Projectors/NEC/Response.cs
+++ Cave/src/DeviceControllers/Projectors/NEC/Response.cs
@@ -53,15 +53,36 @@ namespace Cave.DeviceControllers.Projectors.NEC
         /*  Checksum */
             wild }, nameof(GetSerialInfoSuccess) );
 
+        public static Response VideoMuteOnSuccess = new( new byte[] { 0x22, 0x10, wild, wild, 0x00, wild }, nameof(VideoMuteOnSuccess) );
+        public static Response VideoMuteOffSuccess = new( new byte[] { 0x22, 0x11, wild, wild, 0x00, wild }, nameof(VideoMuteOffSuccess) );
+        public static Response AudioMuteOnSuccess = new( new byte[] { 0x22, 0x12, wild, wild, 0x00, wild }, nameof(AudioMuteOnSuccess) );
+        public static Response AudioMuteOffSuccess = new( new byte[] { 0x22, 0x13, wild, wild, 0x00, wild }, nameof(AudioMuteOffSuccess) );
+
 
         /* Failure responses */
         public static Response PowerOnFailure = new( new byte[] { 0xa2, 0x00, wild, wild, 0x02, wild, wild, wild }, nameof(PowerOnFailure) );
         public static Response PowerOffFailure = new( new byte[] { 0xa2, 0x01, wild, wild, 0x02, wild, wild, wild }, nameof(PowerOffFailure) );
         public static Response SelectInputFailure = new( new byte[] { 0xa2, 0x03, wild, wild, 0x02, wild, wild, wild }, nameof(SelectInputFailure) );
+        public static Response GetStatusFailure = new( new byte[] { 0xa0, 0xbf, wild, wild, 0x02, wild, wild, wild }, nameof(GetStatusFailure) );
         public static Response GetLampInfoFailure = new( new byte[] { 0xa3, 0x96, wild, wild, 0x02, wild, wild, wild }, nameof(GetLampInfoFailure) );
         public static Response GetErrorsFailure = new( new byte[] { 0xa0, 0x88, wild, wild, 0x02, wild, wild, wild }, nameof(GetErrorsFailure) );
+        public static Response GetModelInfoFailure = new( new byte[] { 0xa0, 0x85, wild, wild, 0x02, wild, wild, wild }, nameof(GetModelInfoFailure) );
 
-        public Response( byte[] data, string? name=null ) { Data = data; Name = name; }
+        /* The manual lists the failure code for this command with 0x02 as its fifth byte, same as GetStatusFailure,
+         * however the success response has 0x12 as the fifth byte. I guess I have no way of knowing what's right and
+         * it's unlikely that I ever will. If it's truly the same code, whichever one is encountered first by GetAll()
+         * (may be non-deterministic?) would be returned when getting the name. We obviously don't want that, so ... */
+        public static Response GetSerialInfoFailure = new( new byte[] { 0xa0, 0xbf, wild, wild, 0x12, wild, wild, wild }, nameof(GetSerialInfoFailure) );
+        public static Response VideoMuteOnFailure = new( new byte[] { 0xa2, 0x10, wild, wild, 0x02, wild, wild, wild }, nameof(VideoMuteOnFailure) );
+        public static Response VideoMuteOffFailure = new( new byte[] { 0xa2, 0x11, wild, wild, 0x02, wild, wild, wild }, nameof(VideoMuteOffFailure) );
+        public static Response AudioMuteOnFailure = new( new byte[] { 0xa2, 0x12, wild, wild, 0x02, wild, wild, wild }, nameof(AudioMuteOnFailure) );
+        public static Response AudioMuteOffFailure = new( new byte[] { 0xa2, 0x13, wild, wild, 0x02, wild, wild, wild }, nameof(AudioMuteOffFailure) );
+
+        public Response( byte[] data, string? name=null )
+        {
+            Data = data;
+            Name = name;
+        }
 
         /* Copy constructor for testing */
         public Response( Response other )
@@ -116,15 +137,14 @@ namespace Cave.DeviceControllers.Projectors.NEC
 
         public static IEnumerable<Response> GetAll() =>
             typeof(Response).GetFields( BindingFlags.Public |
-                                        BindingFlags.Static |
-                                        BindingFlags.DeclaredOnly )
+                                        BindingFlags.Static )
                             .Select(field => field.GetValue(null))
                             .Cast<Response>();
 
-        public static Response? FromData( byte[] data )
+        public static string? GetMatchingResponseName( byte[] data )
         {
             var match = GetAll().FirstOrDefault( member => member.Matches(data) );
-            return match;
+            return match?.Name;
         }
 
         public override string ToString()
@@ -136,7 +156,7 @@ namespace Cave.DeviceControllers.Projectors.NEC
                 if( idx < Data.Length - 1 )
                     data += " ";
             }
-            return $"Response: {(Name ?? "[Unknown response]")} [{data}]";
+            return $"Response: {(Name ?? GetMatchingResponseName(this.Data) ?? "[Unknown response]")} [{data}]";
         }
     }
 }

fd2585fff1e23df2a6f560e6d1bcd762dd19af82
tannmatter <tannmatter@gmail.com>
2023-07-17 12:08:02 -0500

    Add AudioMute commands
    Added AudioMuteOn/Off commands and also changed "DisplayMute" on/off to "VideoMute".

--- Cave/src/DeviceControllers/Projectors/NEC/Command.cs
+++ Cave/src/DeviceControllers/Projectors/NEC/Command.cs
@@ -11,14 +11,16 @@ namespace Cave.DeviceControllers.Projectors.NEC
         public static Command PowerOn = new( 1, nameof(PowerOn), new List<byte> { 0x02, 0x00, 0x00, 0x00, 0x00, 0x02 } );
         public static Command PowerOff = new( 2, nameof(PowerOff), new List<byte> { 0x02, 0x01, 0x00, 0x00, 0x00, 0x03 } );
         public static Command SelectInput = new( 3, nameof(SelectInput), new List<byte> { 0x02, 0x03, 0x00, 0x00, 0x02, 0x01 } );
-        public static Command DisplayMuteOn = new( 4, nameof(DisplayMuteOn), new List<byte> { 0x02, 0x10, 0x00, 0x00, 0x00, 0x12 } ) ;
-        public static Command DisplayMuteOff = new( 5, nameof(DisplayMuteOff), new List<byte> { 0x02, 0x11, 0x00, 0x00, 0x00, 0x13 } );
-        public static Command GetStatus = new( 6, nameof(GetStatus), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x01, 0x02, 0xc2 } );
-        public static Command GetInfo = new( 7, nameof(GetInfo), new List<byte> { 0x03, 0x8a, 0x00, 0x00, 0x00, 0x8d } );
-        public static Command GetLampInfo = new( 8, nameof(GetLampInfo), new List<byte> { 0x03, 0x96, 0x00, 0x00, 0x02 } );
-        public static Command GetErrors = new( 9, nameof(GetErrors), new List<byte> { 0x00, 0x88, 0x00, 0x00, 0x00, 0x88 } );
-        public static Command GetModelNumber = new( 10, nameof(GetModelNumber), new List<byte> { 0x00, 0x85, 0x00, 0x00, 0x01, 0x04, 0x8a } );
-        public static Command GetSerialNumber = new( 11, nameof(GetSerialNumber), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x02, 0x01, 0x06, 0xc8 } );
+        public static Command VideoMuteOn = new( 4, nameof(VideoMuteOn), new List<byte> { 0x02, 0x10, 0x00, 0x00, 0x00, 0x12 } ) ;
+        public static Command VideoMuteOff = new( 5, nameof(VideoMuteOff), new List<byte> { 0x02, 0x11, 0x00, 0x00, 0x00, 0x13 } );
+        public static Command AudioMuteOn = new( 6, nameof(AudioMuteOn), new List<byte> { 0x02, 0x12, 0x00, 0x00, 0x00, 0x14 } );
+        public static Command AudioMuteOff = new( 7, nameof(AudioMuteOff), new List<byte> { 0x02, 0x13, 0x00, 0x00, 0x00, 0x15 } );
+        public static Command GetStatus = new( 8, nameof(GetStatus), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x01, 0x02, 0xc2 } );
+        public static Command GetInfo = new( 9, nameof(GetInfo), new List<byte> { 0x03, 0x8a, 0x00, 0x00, 0x00, 0x8d } );
+        public static Command GetLampInfo = new( 10, nameof(GetLampInfo), new List<byte> { 0x03, 0x96, 0x00, 0x00, 0x02 } );
+        public static Command GetErrors = new( 11, nameof(GetErrors), new List<byte> { 0x00, 0x88, 0x00, 0x00, 0x00, 0x88 } );
+        public static Command GetModelNumber = new( 12, nameof(GetModelNumber), new List<byte> { 0x00, 0x85, 0x00, 0x00, 0x01, 0x04, 0x8a } );
+        public static Command GetSerialNumber = new( 13, nameof(GetSerialNumber), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x02, 0x01, 0x06, 0xc8 } );
         public Command( int id, string name, List<byte> data ) : base(id, name) { Data = data; }
 
         public Command( Command original ): base(original)

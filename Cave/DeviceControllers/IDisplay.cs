cb06ba5d79fb258db7f9b646834db4b4718e534a
tannmatter <tannmatter@gmail.com>
2023-07-31 18:44:43 -0500

    Add GetStatus(bool appWantsText)
    This is a hideous hack that will be used by the DisplayTester app for showing a big block of debugging text in its TextView.  Since it's really only for the DisplayTester, I put the method in the IDisplay interface.  I'm not at all happy with this solution, but we'll keep it for now and keep working toward something better.

--- Cave/DeviceControllers/IDisplay.cs
+++ Cave/DeviceControllers/IDisplay.cs
@@ -4,5 +4,9 @@
     {
         Task DisplayOn();
         Task DisplayOff();
+
+        // Temporarily here until I figure out a better place for it
+        // or a better way of doing things?
+        Task GetStatus( bool appWantsText );
     }
 }

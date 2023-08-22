8824b3bee7eb4109891e7a8d5b6c68415aac3715
tannmatter <tannmatter@gmail.com>
2023-08-22 16:07:39 -0500

    Add comments for all methods

--- Cave/Interfaces/IDebuggable.cs
+++ Cave/Interfaces/IDebuggable.cs
@@ -4,8 +4,21 @@ namespace Cave.Interfaces
 {
     public interface IDebuggable
     {
+        /// <summary>
+        /// To be implemented by the IDebuggable device.  Gets a text string
+        /// containing device state used for troubleshooting.
+        /// </summary>
+        /// <returns>A string containing information about the device.</returns>
         Task<string> GetDebugInfo();
 
+        /// <summary>
+        /// Default implementation.  Invokes a method with the required
+        /// parameters and returns the method's result.
+        /// </summary>
+        /// <param name="methodName">Name of the method to invoke</param>
+        /// <param name="parameters">One or more parameters (or an array of
+        /// them) to pass to the method.</param>
+        /// <returns></returns>
         object? InvokeMethod(string methodName, params object?[]? parameters)
         {
             Type thisType = GetType();
@@ -21,6 +34,12 @@ namespace Cave.Interfaces
             return null;
         }
 
+        /// <summary>
+        /// Gets a list of all public methods declared by the debuggable's type.
+        /// </summary>
+        /// <returns>A list of the names of all public methods declared by the
+        /// debuggable's type.
+        /// </returns>
         List<string> GetMethods()
         {
             Type thisType = GetType();

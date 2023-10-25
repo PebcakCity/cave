2046b8401e465ba7980e87c5b90fc6d642addc41
tannmatter <tannmatter@gmail.com>
2023-10-24 19:05:47 -0500

    Move Success/FailureResponseLengths to Command class

--- Cave/DeviceControllers/Projectors/NEC/Command.cs
+++ Cave/DeviceControllers/Projectors/NEC/Command.cs
@@ -2,24 +2,20 @@ using Ardalis.SmartEnum;
 
 namespace Cave.DeviceControllers.Projectors.NEC
 {
+    /// <summary>
+    /// Represents an NEC projector command.  If the command has parameters, they are appended to the end of the command
+    /// using the <see cref="Prepare"/> method.  This method also appends a checksum to the command that must be present
+    /// for successful communication with the device.
+    /// 
+    /// For future versions:
+    /// If I were to redo this, I'd like to have the parameters as an embedded IEnumerable so that they can be easily
+    /// logged.  I might be able to extend it instead... Only 2 commands really have parameters.  I'd like to be able to
+    /// say Logger.Debug($"Sending: {command}") and see something like
+    /// Sending: [Command.SelectInput(Input.HDMI1)] [0x02 0x03 ...]
+    /// </summary>
     public class Command 
         : SmartEnum<Command>
     {
-        public static readonly Command PowerOn = new( 1, nameof(PowerOn), new List<byte> { 0x02, 0x00, 0x00, 0x00, 0x00, 0x02 } );
-        public static readonly Command PowerOff = new( 2, nameof(PowerOff), new List<byte> { 0x02, 0x01, 0x00, 0x00, 0x00, 0x03 } );
-        public static readonly Command SelectInput = new( 3, nameof(SelectInput), new List<byte> { 0x02, 0x03, 0x00, 0x00, 0x02, 0x01 } );
-        public static readonly Command VideoMuteOn = new( 4, nameof(VideoMuteOn), new List<byte> { 0x02, 0x10, 0x00, 0x00, 0x00, 0x12 } ) ;
-        public static readonly Command VideoMuteOff = new( 5, nameof(VideoMuteOff), new List<byte> { 0x02, 0x11, 0x00, 0x00, 0x00, 0x13 } );
-        public static readonly Command AudioMuteOn = new( 6, nameof(AudioMuteOn), new List<byte> { 0x02, 0x12, 0x00, 0x00, 0x00, 0x14 } );
-        public static readonly Command AudioMuteOff = new( 7, nameof(AudioMuteOff), new List<byte> { 0x02, 0x13, 0x00, 0x00, 0x00, 0x15 } );
-        public static readonly Command VolumeAdjust = new( 8, nameof(VolumeAdjust), new List<byte> { 0x03, 0x10, 0x00, 0x00, 0x05, 0x05, 0x00 } );
-        public static readonly Command GetStatus = new( 9, nameof(GetStatus), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x01, 0x02, 0xc2 } );
-        public static readonly Command GetInfo = new( 10, nameof(GetInfo), new List<byte> { 0x03, 0x8a, 0x00, 0x00, 0x00, 0x8d } );
-        public static readonly Command GetLampInfo = new( 11, nameof(GetLampInfo), new List<byte> { 0x03, 0x96, 0x00, 0x00, 0x02 } );
-        public static readonly Command GetErrors = new( 12, nameof(GetErrors), new List<byte> { 0x00, 0x88, 0x00, 0x00, 0x00, 0x88 } );
-        public static readonly Command GetModelNumber = new( 13, nameof(GetModelNumber), new List<byte> { 0x00, 0x85, 0x00, 0x00, 0x01, 0x04, 0x8a } );
-        public static readonly Command GetSerialNumber = new( 14, nameof(GetSerialNumber), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x02, 0x01, 0x06, 0xc8 } );
-
         private readonly List<byte> _data;
 
         public Command( int value, string name, List<byte> data ) : base(name, value) { _data = data; }
@@ -37,11 +33,13 @@ namespace Cave.DeviceControllers.Projectors.NEC
             this.Data.Add(b);
             return this;
         }
+
         public static Command operator +( Command command, byte b )
         {
             command.Data.Add(b);
             return command;
         }
+
         public Command Prepare( params byte[] args )
         {
             int argsAppended = 0;
@@ -56,9 +54,6 @@ namespace Cave.DeviceControllers.Projectors.NEC
             return copy;
         }
 
-        /// <summary>
-        /// Special case for selecting an input by name
-        /// </summary>
         public Command Prepare( string input )
         {
             try
@@ -89,5 +84,57 @@ namespace Cave.DeviceControllers.Projectors.NEC
             }
             return $"Command.{Name}: [{data}]";
         }
+
+        public static readonly Command PowerOn = new( 1, nameof(PowerOn), new List<byte> { 0x02, 0x00, 0x00, 0x00, 0x00, 0x02 } );
+        public static readonly Command PowerOff = new( 2, nameof(PowerOff), new List<byte> { 0x02, 0x01, 0x00, 0x00, 0x00, 0x03 } );
+        public static readonly Command SelectInput = new( 3, nameof(SelectInput), new List<byte> { 0x02, 0x03, 0x00, 0x00, 0x02, 0x01 } );
+        public static readonly Command VideoMuteOn = new( 4, nameof(VideoMuteOn), new List<byte> { 0x02, 0x10, 0x00, 0x00, 0x00, 0x12 } ) ;
+        public static readonly Command VideoMuteOff = new( 5, nameof(VideoMuteOff), new List<byte> { 0x02, 0x11, 0x00, 0x00, 0x00, 0x13 } );
+        public static readonly Command AudioMuteOn = new( 6, nameof(AudioMuteOn), new List<byte> { 0x02, 0x12, 0x00, 0x00, 0x00, 0x14 } );
+        public static readonly Command AudioMuteOff = new( 7, nameof(AudioMuteOff), new List<byte> { 0x02, 0x13, 0x00, 0x00, 0x00, 0x15 } );
+        public static readonly Command VolumeAdjust = new( 8, nameof(VolumeAdjust), new List<byte> { 0x03, 0x10, 0x00, 0x00, 0x05, 0x05, 0x00 } );
+        public static readonly Command GetStatus = new( 9, nameof(GetStatus), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x01, 0x02, 0xc2 } );
+        public static readonly Command GetInfo = new( 10, nameof(GetInfo), new List<byte> { 0x03, 0x8a, 0x00, 0x00, 0x00, 0x8d } );
+        public static readonly Command GetLampInfo = new( 11, nameof(GetLampInfo), new List<byte> { 0x03, 0x96, 0x00, 0x00, 0x02 } );
+        public static readonly Command GetErrors = new( 12, nameof(GetErrors), new List<byte> { 0x00, 0x88, 0x00, 0x00, 0x00, 0x88 } );
+        public static readonly Command GetModelNumber = new( 13, nameof(GetModelNumber), new List<byte> { 0x00, 0x85, 0x00, 0x00, 0x01, 0x04, 0x8a } );
+        public static readonly Command GetSerialNumber = new( 14, nameof(GetSerialNumber), new List<byte> { 0x00, 0xbf, 0x00, 0x00, 0x02, 0x01, 0x06, 0xc8 } );
+
+        public static readonly Dictionary<Command, int> SuccessResponseLengths = new()
+        {
+            { PowerOn, 6 },
+            { PowerOff, 6 },
+            { SelectInput, 7 },
+            { GetStatus, 22 },
+            { GetInfo, 104 },
+            { GetLampInfo, 12 },
+            { GetErrors, 18 },
+            { GetModelNumber, 38 },
+            { GetSerialNumber, 24 },
+            { VideoMuteOn, 6 },
+            { VideoMuteOff, 6 },
+            { AudioMuteOn, 6 },
+            { AudioMuteOff, 6 },
+            { VolumeAdjust, 8 },
+        };
+
+        // pretty much universally 8 bytes but allowing possibility of future variance
+        public static readonly Dictionary<Command, int> FailureResponseLengths = new()
+        {
+            { PowerOn, 8 },
+            { PowerOff, 8 },
+            { SelectInput, 8 },
+            { GetStatus, 8 },
+            { GetInfo, 8 },
+            { GetLampInfo, 8 },
+            { GetErrors, 8 },
+            { GetModelNumber, 8 },
+            { GetSerialNumber, 8 },
+            { VideoMuteOn, 8 },
+            { VideoMuteOff, 8 },
+            { AudioMuteOn, 8 },
+            { AudioMuteOff, 8 },
+            { VolumeAdjust, 8 },
+        };
     }
 }
--- Cave/DeviceControllers/Projectors/NEC/SerialClient.cs
+++ Cave/DeviceControllers/Projectors/NEC/SerialClient.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 using NLog;
@@ -11,12 +8,9 @@ using NLog;
 namespace Cave.DeviceControllers.Projectors.NEC
 {
     /// <summary>
-    /// Client for controlling NEC projectors over a serial port.  A little buggy.
-    /// Timing is a little off, particularly with the NECProjector AwaitPowerOn method.
-    /// Randomly experiences issues related to partial responses (attempts to read info
-    /// that isn't there) while getting PowerState, etc.
-    /// 
-    /// Been trying to improve it little by little.
+    /// Client for controlling NEC projectors over a serial port.  Mostly stable
+    /// as long as timing between sending the command and attempting to read the
+    /// response is not altered.
     /// </summary>
     public class SerialClient : INECClient
     {
@@ -28,43 +22,6 @@ namespace Cave.DeviceControllers.Projectors.NEC
         private readonly SerialPort Port;
         private const int MaxReadSize = 256;
 
-        private readonly Dictionary<Command, int> SuccessResponseLengths = new()
-        {
-            { Command.PowerOn, 6 },
-            { Command.PowerOff, 6 },
-            { Command.SelectInput, 7 },
-            { Command.GetStatus, 22 },
-            { Command.GetInfo, 104 },
-            { Command.GetLampInfo, 12 },
-            { Command.GetErrors, 18 },
-            { Command.GetModelNumber, 38 },
-            { Command.GetSerialNumber, 24 },
-            { Command.VideoMuteOn, 6 },
-            { Command.VideoMuteOff, 6 },
-            { Command.AudioMuteOn, 6 },
-            { Command.AudioMuteOff, 6 },
-            { Command.VolumeAdjust, 8 },
-        };
-
-        // pretty much universally 8 bytes but allowing possibility of future variance
-        private readonly Dictionary<Command, int> FailureResponseLengths = new()
-        {
-            { Command.PowerOn, 8 },
-            { Command.PowerOff, 8 },
-            { Command.SelectInput, 8 },
-            { Command.GetStatus, 8 },
-            { Command.GetInfo, 8 },
-            { Command.GetLampInfo, 8 },
-            { Command.GetErrors, 8 },
-            { Command.GetModelNumber, 8 },
-            { Command.GetSerialNumber, 8 },
-            { Command.VideoMuteOn, 8 },
-            { Command.VideoMuteOff, 8 },
-            { Command.AudioMuteOn, 8 },
-            { Command.AudioMuteOff, 8 },
-            { Command.VolumeAdjust, 8 },
-        };
-
         private SerialClient(string port, int baudrate)
         {
             try
@@ -75,134 +32,50 @@ namespace Cave.DeviceControllers.Projectors.NEC
             }
             catch (IOException ex)
             {
-                Logger.Error($"Error opening port: {ex}");
+                Logger.Error(ex, $"Error opening port: {PortName}");
                 throw;
             }
         }
 
+        /// <summary>
+        /// Creates a <see cref="SerialClient"/> instance with the specified port name and baudrate and tests it to
+        /// ensure the port is readable and writable.
+        /// </summary>
+        /// <param name="port">System port name</param>
+        /// <param name="baudrate">Baudrate</param>
+        /// <returns>A <see cref="Task{T}"/> representing a working <see cref="SerialClient"/> instance</returns>
         public static async Task<SerialClient> Create(string port, int baudrate)
         {
-            try
-            {
-                Logger.Info("Creating new NEC.SerialClient instance.");
-                SerialClient instance = new(port, baudrate);
-                Logger.Info($"Attempting to open port '{port}' with default settings at {baudrate} baud.");
-                await instance.TestConnection();
-                return instance;
-            }
-            catch
-            {
-                throw;
-            }
+            Logger.Info("Creating new NEC.SerialClient instance.");
+            SerialClient instance = new(port, baudrate);
+            Logger.Info($"Attempting to open port '{port}' with default settings at {baudrate} baud.");
+            await instance.TestConnection();
+            return instance;
         }
 
+        // Sends a test command
         private async Task TestConnection()
         {
-            // maybe try to open the port, send a test command and log the result?
             await SendCommandAsync(Command.GetInfo);
         }
 
-        public async Task<Response> SendCommandAsync(Command command)
-        {
-            //return await SendCommandAsyncOriginal(command);
-            //return await SendCommandAsync2(command);
-            return await SendCommandAsync3(command);
-        }
-
-        public async Task<Response> SendCommandAsyncOriginal(Command toSend)
-        {
-            //0. open port
-            //1. do a synchronous write
-            //2. delay for ~ 1/10 to 2/10 of a second to get the whole response
-            //3. do a BaseStream.ReadAsync() OR Port.Read()
-            //4. take the data retrieved, package it in a Response & return it
-            
-            try
-            {
-                var timer = Stopwatch.StartNew();
-                byte[] responseBytes = new byte[MaxReadSize];
-
-                Port.Open();
-                Logger.Debug($"Sending command: {toSend}");
-                byte[] cmdBytes = toSend.Data.ToArray();
-                Port.Write(cmdBytes, 0, cmdBytes.Length);
-
-                await Task.Delay(100);
-
-                //int bytesRead = await Port.BaseStream.ReadAsync(responseBytes, 0, responseBytes.Length);
-                int bytesRead = Port.Read(responseBytes, 0, responseBytes.Length);
-                Logger.Debug($"Read {bytesRead} bytes.");
-                Response response = new(responseBytes[0..bytesRead]);
-                Logger.Debug($"Received response: {response}");
-                timer.Stop();
-                Logger.Debug($"Took {timer.ElapsedMilliseconds}ms");
-                return response;
-            }
-            finally
-            {
-                Port.Close();
-            }
-        }
-
-        public async Task<Response> SendCommandAsync2(Command toSend)
-        {
-            // Have read that CancellationToken doesn't work properly with SerialPort.BaseStream... testing
-            using ( CancellationTokenSource cts = new() )
-            {
-                try
-                {
-                    var timer = Stopwatch.StartNew();
-                    Port.Open();
-
-                    Logger.Debug($"Sending command: {toSend}");
-                    byte[] cmdBytes = toSend.Data.ToArray();
-                    await Port.BaseStream.WriteAsync(cmdBytes);
-
-                    await Task.Delay(100);
-
-                    byte[] responseBytes = new byte[MaxReadSize];
-                    cts.CancelAfter(100);
-                    int bytesRead = await Port.BaseStream.ReadAsync(
-                        responseBytes.AsMemory(0, responseBytes.Length), cts.Token);
-                    Logger.Debug($"Read {bytesRead} bytes.");
-
-                    Response response = new(responseBytes[0..bytesRead]);
-                    Logger.Debug($"Received response: {response}");
-                    timer.Stop();
-                    Logger.Debug($"Took {timer.ElapsedMilliseconds}ms");
-                    return response;
-                }
-                catch(OperationCanceledException) 
-                {
-                    throw new TimeoutException("Operation timed out");
-                }
-                finally
-                {
-                    Port.Close();
-                }
-            }
-        }
-
-        /**
-         * Not much of a discernible difference between the two on Windows.  I did notice something kinda interesting.
-         * The pattern of behavior regarding when exceptions are thrown was pretty much identical but this time I
-         * noticed that one of the exceptions said that the serial port was already open when trying to open it.
-         * It's during that whole AwaitPowerOn business so it's likely a timing issue between waiting 100 ms in the read
-         * op and calling multiple read ops in a row.  Should investigate and see if this is what's happening in the
-         * synchronous read version that just uses Task.Delay as the only async component...
-         * 
-         * Other things to try for improving stability/timing on this:
-         * 1) Maybe try to use locks somehow to prevent concurrent accesses to the serial port?
-         * 
-         * 2) The solution below -- kinda ugly but it seems to work.  Since we know from the NEC documentation how long
-         *  each response is supposed to be, whether it's an error or not, and also what an error response starts with
-         *  (byte with upper 4 bits 'a', 1010)... we keep two collections of expected response lengths for each command,
-         *  one for failure, one for success... SendCommandAsync checks the command being sent, sends it, waits a very
-         *  short amount of time, gets the firstByte byte to know whether it succeeded or not (and how many more bytes to
-         *  get) then sets up a read operation to get the rest and returns it.
-         */
-        
-        public async Task<Response> SendCommandAsync3( Command command )
+        /// <summary>
+        /// Sends the <see cref="Command"/> <paramref name="command"/> and returns the <see cref="Response"/> from the
+        /// device.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task{T}"/> representing the <see cref="Response"/> from the device.
+        /// </returns>
+        /// <exception cref="EndOfStreamException">
+        /// If no data is read because the end of the stream has been reached
+        /// </exception>
+        /// <exception cref="InvalidDataException">
+        /// If the response from the device cannot be interpreted because of data corruption
+        /// </exception>
+        /// <exception cref="TimeoutException">
+        /// If a read timeout occurs waiting for the response
+        /// </exception>
+        public async Task<Response> SendCommandAsync( Command command )
         {
             using ( var cts = new CancellationTokenSource() )
             {
@@ -213,6 +86,8 @@ namespace Cave.DeviceControllers.Projectors.NEC
                     Port.Open();
 
                     Logger.Debug($"Sending command: {command}");
+
+                    // Send command.
                     byte[] cmdBytes = command.Data.ToArray();
                     Port.Write(cmdBytes, 0, cmdBytes.Length);
 
@@ -225,17 +100,13 @@ namespace Cave.DeviceControllers.Projectors.NEC
                     if ( firstByte == -1 )
                         throw new EndOfStreamException("Unexpected end of stream");
 
-                    // Get length of expected response based on whether first
-                    // byte indicates command success or failure
-                    //int expectedLength = ((firstByte>>4)==0x0a) ?
-                    //    FailureResponseLengths[command] :
-                    //    SuccessResponseLengths[command];
-
-                    // Check for possible data corruption 
+                    // Get length of expected response (or throw an exception)
+                    // based on whether the first byte indicates command failure,
+                    // success, or neither (indicating data corruption)
                     int expectedLength = (firstByte >> 4) switch
                     {
-                        0x0a => FailureResponseLengths[command],
-                        0x02 => SuccessResponseLengths[command],
+                        0x0a => Command.FailureResponseLengths[command],
+                        0x02 => Command.SuccessResponseLengths[command],
                         _ => throw new InvalidDataException("Bad response from device")
                     };
 
@@ -253,25 +124,30 @@ namespace Cave.DeviceControllers.Projectors.NEC
                     Logger.Debug($"Response: {response}");
                     return response;
                 }
+                // ReadAsync timeout (CancellationTokenSource expired)
                 catch ( OperationCanceledException )
                 {
-                    throw new TimeoutException("Read operation timed out") { 
+                    throw new TimeoutException("Serial operation timed out")
+                    { 
                         Data = { { "Command", command.Name } }
                     };
                 }
-                catch ( TimeoutException te )
-                {
-                    te.Data.Add("Command", command.Name);
-                    throw;
-                }
+                // (firstByte >> 4) != 0x0a or 0x02
                 catch ( InvalidDataException ide )
                 {
                     responseBytes = new byte[MaxReadSize];
                     responseBytes[0] = (byte)firstByte;
+                    // May throw TimeoutException and be caught below?
                     int bytesRead = Port.Read(responseBytes, 1, responseBytes.Length-1);
                     Response junkResponse = new(responseBytes[0..(bytesRead+1)]);
                     ide.Data.Add("Command", command.Name);
                     ide.Data.Add("Response", junkResponse.ToString());
+                    throw;
+                }
+                // Synchronous Write/Read timeout
+                catch ( TimeoutException te )
+                {
+                    te.Data.Add("Command", command.Name);
                     throw;
                 }
                 finally
